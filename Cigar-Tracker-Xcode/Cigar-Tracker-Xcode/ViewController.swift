c69693f607d8fe8739631ea205b201dd3f9d8bf7
AustinPotts <pottsevolvements@gmail.com>
2020-03-20 20:16:17 -0400

    Xcode Project Configured

---
+++ Cigar-Tracker-Xcode/Cigar-Tracker-Xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cigar-Tracker-Xcode
+//
+//  Created by Austin Potts on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
